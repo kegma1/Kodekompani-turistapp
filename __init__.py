58eda1e2330386b9ed8fd6be79c006b5a83bc62c
kegma1 <61615726+kegma1@users.noreply.github.com>
2024-02-19 12:31:10 +0100

    wee wuu shit more boilerplate

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import secrets
 
 
@@ -8,7 +8,7 @@ app.secret_key = secrets.token_urlsafe(16)
 
 @app.route("/", methods = ["GET"])
 def index():
-    return "Hallo, Verden!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
