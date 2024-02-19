8b4b0234d54b2900d65b74b49c78a2494131e23a
kegma1 <61615726+kegma1@users.noreply.github.com>
2024-02-19 12:24:11 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import secrets
+
+
+app = Flask(__name__)
+
+app.secret_key = secrets.token_urlsafe(16)
+
+@app.route("/", methods = ["GET"])
+def index():
+    return "Hallo, Verden!"
