cc01fce6346322bc9f8a12a9ec151ca12a7f578e
Mikael Isaksen <mikaellunde2003@hotmail.com>
2024-02-22 22:03:35 +0100

    login finito + admin start

--- routes/admin.py
+++ routes/admin.py
@@ -1,1 +1,12 @@
+from __main__ import app, redirect, render_template, url_for
 from utils import Friend, User, UserAttraction, UserAchievement, db_session
+
+@app.route("/admin", methods = ["GET", "POST"]) # Needs more checks for admin privileges in the future
+def admin():
+    
+    return render_template("admin.html", 
+                           friends = db_session.query(Friend).all(), 
+                           users = db_session.query(User).all(), 
+                           user_attractions = db_session.query(UserAttraction).all(), 
+                           user_achievement = db_session.query(UserAchievement).all(), 
+                           title = "Admin Page")
