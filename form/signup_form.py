107752464f793eed0be03ddcd99a7f607a3ff9c4
Mikael Isaksen <mikaellunde2003@hotmail.com>
2024-02-23 12:31:21 +0100

    first & full login

--- form/signup_form.py
+++ form/signup_form.py
@@ -2,7 +2,9 @@ from wtforms import Form, StringField, PasswordField, EmailField, validators, Va
 from utils import db_session, User
 
 class SignUpForm(Form):
-    username = StringField("Username", [validators.data_required(), validators.Length(min=1, max=255)])
+    username = StringField("Username", [validators.data_required(), validators.Length(min=1, max=50, message= "Username length must be between 1 and 50 charecters.")])
+    first_name = StringField("First name", [validators.data_required(), validators.Length(min=1, max=50, message= "First name required.")])
+    last_name = StringField("Last name", [validators.data_required(), validators.Length(min=1, max=50, message= "Last name required.")])
     email = EmailField("E-mail address", [validators.data_required()])
     password = PasswordField("Password", [validators.data_required(), validators.equal_to("confirm", message="Passwords must match"), validators.Length(min = 8, max=255)])
     confirm = PasswordField("Confirm password")
