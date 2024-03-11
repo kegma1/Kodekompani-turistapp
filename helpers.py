0e5abdb7371735da1153d034bace65c5e5f39f40
Jonnius00 <itsoft@gmail.com>
2024-03-11 13:30:45 +0100

    4. add flash notifications

--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
 ''''a module for utility functions or helper functions
 '''
 from functools import wraps
-from flask import redirect, session, url_for
+from flask import flash, redirect, session, url_for
 
 def require_login(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "is_logged_in" not in session or not session["is_logged_in"]:
+            flash('Available only authorized users.', 'danger')
             return redirect(url_for('index'))
         return f(*args, **kwargs)
     return decorated_function
