8a3f2ae7a974ea27013f41351ada25c0f0e67f04
kegma1 <61615726+kegma1@users.noreply.github.com>
2024-03-12 14:27:21 +0100

    wups glemte xp

--- db_utils.py
+++ db_utils.py
@@ -36,6 +36,7 @@ class User(Base):
     encrypted_password = Column(String(255), nullable=False)
     isAdmin = Column(Boolean)
     isDeleted = Column(Boolean, default= False)
+    xp_collected = Column(Integer)
     _profile_pic = Column("profile_pic" , LargeBinary)
     attractions = relationship("Attraction", secondary="user_attractions")
     achievements = relationship("Achievement", secondary="user_achievements")
