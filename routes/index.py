ccd671b39c567e09aa4e3d1c9ec50069896842b9
kegma1 <61615726+kegma1@users.noreply.github.com>
2024-03-16 21:58:18 +0100

    you now get you top 5 achivements on your homepage

--- routes/index.py
+++ routes/index.py
@@ -12,6 +12,8 @@ def index():
     fullname = ""
     xp = 0; user_status = "Newbie in city"
 
+    top_5_achievements = None
+
     if "is_logged_in" in session and session["is_logged_in"]:
         data = db_session.query(User).filter_by(username=session["user"]).first()
         bio = data.bio
@@ -29,9 +31,16 @@ def index():
         is_logged_in = True
         username = session["user"]
         fullname = data.full_name
+
+        # Finding top 5 achievements
+        if data.achievements is not []:
+            top_5_achievements = sorted(data.achievements, key=lambda x: x.xp_reward, reverse=True)[:5]
+
+                
+
         
     attractions = db_session.query(Attraction).all()
 
     return render_template("index.html", title="Home page", pfp=pfp, bio = bio or None, 
                            is_logged_in=is_logged_in, username=username, xp=xp, user_status=user_status,
-                           fullname = fullname, attractions=attractions)
+                           fullname = fullname, attractions=attractions, t5achievements=top_5_achievements)
