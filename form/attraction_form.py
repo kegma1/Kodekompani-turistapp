94927481b92581f075b51631df914c4b685728ee
kegma1 <61615726+kegma1@users.noreply.github.com>
2024-03-18 20:48:17 +0100

    added maps tihi

--- form/attraction_form.py
+++ form/attraction_form.py
@@ -2,18 +2,20 @@ from wtforms import StringField, IntegerField, validators, SelectField, TextArea
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+
 class AttractionForm(FlaskForm):
     def __init__(self, data_required, formdata=..., **kwargs):
         super().__init__(formdata, **kwargs)
+        self.location_coordinates.validators = [validators.Regexp(r'^\d+\.\d+,\s?\d+\.\d+$', message="invalid coordinates format.")]
         if data_required:
-            self.image.validators = [FileRequired()]
+            self.image.validators = [FileRequired(), FileAllowed(["png", "jpg", "jpeg", "jfif", "webp"], "Only images")]
             self.name.validators = [validators.data_required()]
             self.description.validators = [validators.data_required()]
             self.category.validators = [validators.data_required()]
             self.age_recommendation.validators = [validators.data_required()]
             self.address.validators = [validators.data_required()]
             self.keywords.validators = [validators.data_required()]
-    
+            self.location_coordinates.validators = [validators.data_required(), validators.Regexp(r'^\d+\.\d+,\s?\d+\.\d+$', message="invalid coordinates format.")]
     name = StringField("Name")
     description = TextAreaField("Description")
     category = StringField("Category")
@@ -24,3 +26,6 @@ class AttractionForm(FlaskForm):
     keywords = StringField("keywords")
 
     image = FileField("Image banner", validators= [FileAllowed(["png", "jpg", "jpeg", "jfif", "webp"], "Only images")])
+
+
+
--- routes/map_test.py
+++ routes/map_test.py
@@ -1,6 +1,17 @@
 from __main__ import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/map_test')
 def map_test():
-    return render_template("map_test.html", title="TEST")
+    lat = 68.0
+    lon = 17.0
+    zoom = 4
+    if request.args.get('lon'):
+        lon = float(request.args.get('lon'))
+    if request.args.get('lat'):
+        lat = float(request.args.get('lat'))
+    if request.args.get('zoom'):
+        zoom = float(request.args.get('zoom'))
+    return render_template("map_test.html", lon = lon, 
+                           lat = lat, 
+                           zoom = zoom)
