83efd7c86de9e45b1c217b380ef7a14480c2cb22
iZetzo <hj4lmar21@gmail.com>
2024-04-07 20:13:38 +0200

    Friendlist works Smileyface

--- routes/following_list.py
+++ routes/following_list.py
@@ -2,19 +2,18 @@ from __main__ import app
 from flask import render_template
 from db_utils import db_session, User
 
-@app.route('/followers_list', methods=["GET"])
-def followers_list(follow):
-    followers = get_follow_info(follow, 'user_id')
-        
-    return render_template()
+@app.route('/followers_list/<userid>', methods=["GET"])
+def followers_list(userid):
+    user = db_session.query(User).filter_by(id = userid).first()
+    return render_template("follow_list.html", userlist = user.followers, previoususer = user.username)
 
 
-@app.route('/following_list', methods=["GET"])
-def following_list(follow):
-    following = get_follow_info(follow, 'friend_id')
+@app.route('/following_list/<userid>', methods=["GET"])
+def following_list(userid):
+    user = db_session.query(User).filter_by(id = userid).first()
+    return render_template("follow_list.html", userlist = user.following, previoususer = user.username)
 
-
-def get_follow_info(follow, id):
-    for _ in follow:
-        x = db_session.query(User).filter_by(id = id).all()
-    return x
+# def get_follow_info(follow, id):
+#     for _ in follow:
+#         x = db_session.query(User).filter_by(id = id).all()
+#     return x
