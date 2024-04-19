71e3bf5796e576356c06e17d5a56bc4661d1c2f0
Mikael Isaksen <mikaellunde2003@hotmail.com>
2024-04-20 00:35:52 +0200

    fixed attraction_lists.py when not logged in

--- routes/attractions_list.py
+++ routes/attractions_list.py
@@ -1,14 +1,16 @@
 from __main__ import app
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session
 from db_utils import Attraction, User, db_session
-from libs.helpers import get_user_age, require_login
+from libs.helpers import get_user_age
 
-@require_login
 @app.route("/attractions/list", methods = ["GET", "POST"])
-def attractions_list(): 
-    user = db_session.query(User).filter_by(username=session["user"]).first()
-    age = get_user_age(user)
-    attractions = db_session.query(Attraction).filter(Attraction.age_recommendation <= age).all()
+def attractions_list():
+    try: 
+        user = db_session.query(User).filter_by(username=session["user"]).first()
+        age = get_user_age(user)
+        attractions = db_session.query(Attraction).filter(Attraction.age_recommendation <= age).all()
+    except:
+        attractions = db_session.query(Attraction).filter(Attraction.age_recommendation == 0).all()
     
     return render_template("view_attractions.html", 
                            attractions = attractions)
